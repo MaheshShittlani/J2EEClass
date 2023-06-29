4e6999fd811cff74926fde3a28d798d25880d627
Mahesh Shittlani <maheshshittlani@gmail.com>
2023-06-29 10:24:37 +0530

    App1 & App2

---
+++ App1/src/HelloServlet.java
@@ -0,0 +1,17 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class HelloServlet extends HttpServlet {
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+
+        PrintWriter out =  response.getWriter();
+        out.println("<h1>Hello World</h1>");
+        out.close();
+    }
+}
